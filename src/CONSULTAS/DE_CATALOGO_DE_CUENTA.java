53a69a446cd8102a52232197a5efa5578f74dfb1
mathew1704 <mathew2004montero@gmail.com>
2023-08-09 03:28:15 -0400

    Proyecto

--- src/CONSULTAS/DE_CATALOGO_DE_CUENTA.java
+++ src/CONSULTAS/DE_CATALOGO_DE_CUENTA.java
@@ -4,15 +4,20 @@
  */
 package CONSULTAS;
 
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author garci
  */
 public class DE_CATALOGO_DE_CUENTA extends javax.swing.JFrame {
 
-    /**
-     * Creates new form DE_CATALOGO_DE_CUENTA
-     */
+    public DefaultTableModel TablaM;
     public DE_CATALOGO_DE_CUENTA() {
         initComponents();
     }
@@ -26,21 +31,176 @@ public class DE_CATALOGO_DE_CUENTA extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        PanelAzul = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tablaRegistro = new javax.swing.JTable();
+        btnSalir = new javax.swing.JButton();
+        btnconsultar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        PanelAzul.setBackground(new java.awt.Color(0, 153, 255));
+
+        jLabel1.setFont(new java.awt.Font("Century Gothic", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("CONSULTA DE Catalogo de Cuentas");
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        tablaRegistro.setFont(new java.awt.Font("Century Gothic", 0, 14)); // NOI18N
+        tablaRegistro.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
+            },
+            new String [] {
+                "Usuario", "Contraseña", "Nivel", "Nombre", "Apellidos", "Email"
+            }
+        ));
+        jScrollPane1.setViewportView(tablaRegistro);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 900, 410));
+
+        btnSalir.setBackground(new java.awt.Color(160, 171, 176));
+        btnSalir.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
+        btnSalir.setForeground(new java.awt.Color(255, 255, 255));
+        btnSalir.setText("SALIR");
+        btnSalir.setBorder(null);
+        btnSalir.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnSalir.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnSalirMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnSalirMouseExited(evt);
+            }
+        });
+        btnSalir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSalirActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 430, 150, 50));
+
+        btnconsultar.setBackground(new java.awt.Color(160, 171, 176));
+        btnconsultar.setFont(new java.awt.Font("Century Gothic", 0, 18)); // NOI18N
+        btnconsultar.setForeground(new java.awt.Color(255, 255, 255));
+        btnconsultar.setText("CONSULTAR");
+        btnconsultar.setBorder(null);
+        btnconsultar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnconsultar.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnconsultarMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnconsultarMouseExited(evt);
+            }
+        });
+        btnconsultar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnconsultarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnconsultar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 430, 150, 50));
+
+        javax.swing.GroupLayout PanelAzulLayout = new javax.swing.GroupLayout(PanelAzul);
+        PanelAzul.setLayout(PanelAzulLayout);
+        PanelAzulLayout.setHorizontalGroup(
+            PanelAzulLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        PanelAzulLayout.setVerticalGroup(
+            PanelAzulLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(PanelAzulLayout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 494, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(PanelAzul, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(PanelAzul, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnSalirMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalirMouseEntered
+        btnSalir.setBackground(Color.red);
+    }//GEN-LAST:event_btnSalirMouseEntered
+
+    private void btnSalirMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalirMouseExited
+        btnSalir.setBackground(new Color(160, 171, 176));
+    }//GEN-LAST:event_btnSalirMouseExited
+
+    private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
+        GENERAL_CONSULTAS e = new GENERAL_CONSULTAS();
+        e.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_btnSalirActionPerformed
+
+    private void btnconsultarMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnconsultarMouseEntered
+        btnconsultar.setBackground(Color.red);
+    }//GEN-LAST:event_btnconsultarMouseEntered
+
+    private void btnconsultarMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnconsultarMouseExited
+        btnconsultar.setBackground(new Color(160, 171, 176));
+    }//GEN-LAST:event_btnconsultarMouseExited
+
+    private void btnconsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnconsultarActionPerformed
+        TablaM.setRowCount(0);
+        boolean filas = false;
+
+        String user, passw, nivel, nomb, apell, email;
+        File f = new File("Usuarios.txt");
+
+        try {
+            if (!f.exists()) {
+                JOptionPane.showMessageDialog(rootPane, "El archivo no existe");
+            } else {
+                Scanner s = new Scanner(f);
+
+                while (s.hasNextLine()) {
+                    String linea = s.nextLine();
+                    Scanner s1 = new Scanner(linea);
+
+                    s1.useDelimiter("\\s*;\\s*");
+
+                    user = s1.next();
+                    passw = s1.next();
+                    nivel = s1.next();
+                    nomb = s1.next();
+                    apell = s1.next();
+                    email = s1.next();
+
+                    TablaM.addRow(new Object[]{user, passw, nivel, nomb, apell, email});
+
+                    filas = true;
+                }
+                s.close();
+
+                if (!filas) {
+                    JOptionPane.showMessageDialog(rootPane, "No se encontró ningún registro");
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error al abrir el archivo");
+        }
+    }//GEN-LAST:event_btnconsultarActionPerformed
 
     /**
      * @param args the command line arguments
@@ -78,5 +238,12 @@ public class DE_CATALOGO_DE_CUENTA extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel PanelAzul;
+    private javax.swing.JButton btnSalir;
+    private javax.swing.JButton btnconsultar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tablaRegistro;
     // End of variables declaration//GEN-END:variables
 }
